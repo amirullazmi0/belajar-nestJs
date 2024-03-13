3dad037e65c7ed0999027510cf22bae0ad378887
Amirul Azmi <amirullazmi0@gmail.com>
2024-03-13 10:54:23 +0700

    first-commit

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Header, HttpCode, Post, Query, Req, Res } from '@nestjs/common';
+import { Request, Response } from "express";
+import { json } from 'stream/consumers';
+
+@Controller('/api/user')
+export class UserController {
+    @Post()
+    post(): String {
+        return 'POST'
+    }
+
+    @Get('/hello')
+    async sayHello(@Query("name") name: String, @Query("age") age: number,) {
+        try {
+            return `Hello ${name}, umur anda ${age}`
+        } catch (error) {
+
+        }
+    }
+
+    @Get('/set-cookie')
+    setCookie(@Query('name') name: string, @Res() response: Response) {
+        response.cookie('name', name)
+        response.status(200).json({
+            message: "set cookie success"
+        })
+    }
+    @Get('/get-cookie')
+    getCookie(@Res() response: Response, @Req() request: Request) {
+        response.json({
+            cookie: request.cookies['name']
+        })
+    }
+
+    @Get('/all')
+    @Header("content-type", "application/json")
+    @HttpCode(200)
+    all() {
+        try {
+            return {
+                data: "asdasdasdas"
+            }
+        } catch (error) {
+
+        }
+    }
+}
