3e1fcdf986baef7151c87eb26132c912a115f76b
amirullazmi0 <amirullazmi0@gmail.com>
2024-03-14 13:43:14 -0700

    prisma-install

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
 import * as mustache from 'mustache-express'
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser('Rahasia'));
 
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService)
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
